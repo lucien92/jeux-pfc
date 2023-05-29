d728ed30e4eecc7183f9876514c74f263448c2f0
lucien92 <lucien.rondier@student-cs.fr>
2023-05-29 11:15:37 +0200

    ini

---
+++ src/routes/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+	import { onMount } from 'svelte';
+	import 'tailwindcss/tailwind.css';
+	let isTextVisible = false;
+  
+	onMount(() => {
+	  setTimeout(() => {
+		isTextVisible = true;
+	  }, 500);
+	});
+  </script>
+  
+  <style>
+	.container {
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: center;
+	  min-height: 100vh;
+	  background-image: url('image_fond.jpeg');
+	  background-size: cover;
+	}
+  
+	.text {
+	  font-size: 24px;
+	  text-align: center;
+	  color: black;
+	  font-weight: bold;
+	  opacity: 0;
+	  transition: opacity 500ms ease-in-out;
+	  white-space: nowrap;
+	  overflow: hidden;
+	}
+  
+	.text-visible {
+	  opacity: 1;
+	  transition-delay: 500ms;
+	}
+  </style>
+  
+  <div class="container">
+	<h1 class="text transition-opacity duration-500 ease-in-out delay-500" class:text-visible={isTextVisible}>
+	  <span class="animate-appear-letter">
+		Pierre Feuille Ciseaux
+	  </span>
+	</h1>
+	<p class="text transition-opacity duration-500 ease-in-out delay-1000" class:text-visible={isTextVisible}>
+	  <span class="animate-appear-letter">
+		Pierre-papier-ciseaux est un jeu effectué avec les mains et opposant deux joueurs.
+	  </span>
+	</p>
+	<p class="text transition-opacity duration-1000 ease-in-out delay-1500" class:text-visible={isTextVisible}>
+		<span class="animate-appear-letter">
+		 Il possède de nombreux noms alternatifs, notamment en variant l'ordre des termes
+		</span>
+	</p>
+	<p class="text transition-opacity duration-1500 ease-in-out delay-2000" class:text-visible={isTextVisible}>
+		<span class="animate-appear-letter">
+		 ou en remplaçant certains mots comme « papier » par « feuille » ou « pierre » par
+		</span>
+	</p>
+	<p class="text transition-opacity duration-25000 ease-in-out delay-30000" class:text-visible={isTextVisible}>
+		<span class="animate-appear-letter">
+		« caillou » ou « roche ». Le terme chifoumi est également une appellation courante.		</span>
+	</p>
+  </div>
+  
+  
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { onMount } from 'svelte';
+	import 'tailwindcss/tailwind.css';
+  
+	let choices = ['Pierre', 'Feuille', 'Ciseaux'];
+	let playerChoice = '';
+	let computerChoice = '';
+	let result = '';
+  
+	const playGame = (choice) => {
+	  playerChoice = choice;
+	  computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  
+	  if (playerChoice === computerChoice) {
+		result = "Égalité !";
+	  } else if (
+		(playerChoice === 'Pierre' && computerChoice === 'Ciseaux') ||
+		(playerChoice === 'Feuille' && computerChoice === 'Pierre') ||
+		(playerChoice === 'Ciseaux' && computerChoice === 'Feuille')
+	  ) {
+		result = "Gagné !";
+	  } else {
+		result = "Perdu !";
+	  }
+	};
+  </script>
+  
+  <style>
+	.container {
+	  display: flex;
+	  flex-direction: column;
+	  justify-content: center;
+	  align-items: center;
+	  min-height: 100vh;
+	}
+  
+	.button {
+	  padding: 0.5rem 1rem;
+	  margin: 0.5rem;
+	  border: none;
+	  border-radius: 0.25rem;
+	  background-color: #4a5568;
+	  color: #fff;
+	  font-size: 1rem;
+	  cursor: pointer;
+	}
+  </style>
+  
+  <div class="container">
+	<h5>Pierre Feuille Ciseaux</h5>
+	<p>Choisissez votre coup :</p>
+	<div>
+	  <button class="button" on:click={() => playGame('Pierre')}>Pierre</button>
+	  <button class="button" on:click={() => playGame('Feuille')}>Feuille</button>
+	  <button class="button" on:click={() => playGame('Ciseaux')}>Ciseaux</button>
+	</div>
+	{#if playerChoice !== ''}
+	  <p>Vous avez choisi : {playerChoice}</p>
+	  <p>L'ordinateur a choisi : {computerChoice}</p>
+	  <p>Résultat : {result}</p>
+	{/if}
+  </div>
+  
