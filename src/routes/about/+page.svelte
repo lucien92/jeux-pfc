5b0f11c1eac001b4136bb0dd11f451f9c0d9be97
lucien92 <lucien.rondier@student-cs.fr>
2023-05-29 12:02:58 +0200

    ini

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,8 @@
 <script>
 	import { onMount } from 'svelte';
+	import rock from '$lib/images/rock.svg';
+	import leaf from '$lib/images/leaf.svg';
+	import cissors from '$lib/images/cissors.svg';
 	import 'tailwindcss/tailwind.css';
   
 	let choices = ['Pierre', 'Feuille', 'Ciseaux'];
@@ -34,6 +37,20 @@
 	  min-height: 100vh;
 	}
   
+	.title {
+	  font-size: 2rem;
+	  color: red;
+	  font-style: italic;
+	  margin-bottom: 1rem;
+	}
+  
+	.banner {
+	  background-color: yellow;
+	  padding: 1rem;
+	  margin-bottom: 1rem;
+	  border-radius: 0.25rem;
+	}
+  
 	.button {
 	  padding: 0.5rem 1rem;
 	  margin: 0.5rem;
@@ -43,21 +60,45 @@
 	  color: #fff;
 	  font-size: 1rem;
 	  cursor: pointer;
+	  display: flex;
+	  align-items: center;
+	}
+  
+	.button img {
+	  width: 1.5rem;
+	  height: 1.5rem;
+	  fill: currentColor;
+	  margin-right: 0.5rem;
+	}
+  
+	.result {
+	  font-weight: bold;
+	  margin-top: 1rem;
 	}
   </style>
   
   <div class="container">
-	<h5>Pierre Feuille Ciseaux</h5>
-	<p>Choisissez votre coup :</p>
+	<h1 class="title">Pierre Feuille Ciseaux</h1>
+	<div class="banner">
+	  <p><em>Choisissez votre coup :</em></p>
+	</div>
 	<div>
-	  <button class="button" on:click={() => playGame('Pierre')}>Pierre</button>
-	  <button class="button" on:click={() => playGame('Feuille')}>Feuille</button>
-	  <button class="button" on:click={() => playGame('Ciseaux')}>Ciseaux</button>
+	  <button class="button" on:click={() => playGame('Pierre')}>
+		<img src={rock} alt="Rock" />
+		<strong>Pierre</strong>
+	  </button>
+	  <button class="button" on:click={() => playGame('Feuille')}>
+		<img src={leaf} alt="Leaf" />
+		<strong>Feuille</strong>
+	  </button>
+	  <button class="button" on:click={() => playGame('Ciseaux')}>
+		<img src={cissors} alt="Cissors" />
+		<strong>Ciseaux</strong>
+	  </button>
 	</div>
 	{#if playerChoice !== ''}
 	  <p>Vous avez choisi : {playerChoice}</p>
 	  <p>L'ordinateur a choisi : {computerChoice}</p>
-	  <p>Résultat : {result}</p>
+	  <p class="result">Résultat : {result}</p>
 	{/if}
-  </div>
-  
+</div>  
